8ebeafa7c1604585983294ce795b65a9caec1278
hellovolcano <valerie.gleason@gmail.com>
2022-09-30 07:50:29 -0500

    bootstrapping repo

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+
+function Nav() {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Somewhere else</li>
+                <li>Another somewhere else</li>
+            </ul>
+
+        </nav>
+    )
+}
+export default Nav
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Nav from '../components/Nav'
+
+function Home() {
+    return(
+        <>  
+            <Nav />
+            <h1>Default App</h1>
+        </>
+    )
+}
+
+export default Home
