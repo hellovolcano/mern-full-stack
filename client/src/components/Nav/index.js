1b52d0fda720b9bf4f497221861d1993650319bb
hellovolcano <valerie.gleason@gmail.com>
2022-10-05 17:23:48 -0500

    adding seeder logic and adding initial nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,35 @@
+import { Link } from 'react-router-dom'
+import Auth from '../../utils/auth'
 
-function Nav() {
+const Nav = () => {
+    const logout = event => {
+        event.preventDefault()
+        Auth.logout()
+        // TODO: Remove this console.log once feature fully implemented
+        console.log("logout click")
+    }
+
+    // TODO: delete this tester variable once we have Auth implemented on the front end
+    const loggedIn = true
     return(
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>Somewhere else</li>
-                <li>Another somewhere else</li>
-            </ul>
-
+            {/* TODO: Change this to Auth.loggedIn() once we've imported Auth */}
+            {loggedIn ? (
+                <>
+                    <Link to="/profile">Me</Link>
+                    <a href="/" onClick={logout}>Logout</a>
+                </>
+            ) : (
+                <>
+                    <Link to="/signup">Sign up</Link>
+                    <Link to="/login">Login</Link>
+                </>
+            )}
+            
+            
+            
         </nav>
     )
 }
+
 export default Nav
