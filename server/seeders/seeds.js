633694fdd9de165948e4203890d1873e05cf13a7
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-10-04 20:26:11 -0500

    added seeds

---
+++ server/seeders/seeds.js
@@ -0,0 +1,27 @@
+[
+    {
+        "username": "CarlSandburg",
+        "email": "carlsandburg@test.com",
+        "poemText": "I spot the hills With yellow balls in autumn. I light the prairie cornfields Orange and tawny gold clusters And I am called pumpkins. On the last of October When dusk is fallen Children join hands And circle round me Singing ghost songs And love to the harvest moon; I am a jack-o'-lantern With terrible teeth And the children know I am fooling."
+    },
+    {
+        "username": "Anonymous",
+        "email": "anonymous@test.com",
+        "poemText": "White sheep, white sheep, On a blue hill, When the wind stops, You all stand still. When the wind blows, You walk away slow. White sheep, white sheep, Where do you go?"
+    },
+    {
+        "username": "ChristinaRossetti",
+        "email": "christinarossetti@test.com",
+        "poemText": "Who has seen the wind? Neither I nor you. But when the leaves hang trembling, The wind is passing through. Who has seen the wind? Neither you nor I. But when the trees bow down their heads, The wind is passing by."
+    },
+    {
+        "username": "ModernMasaoka",
+        "email": "modernmasaoka@test.com",
+        "poemText": "Crashing waves and spray; no rashguard means you rash hard; your middle, a sunset"
+    },
+    {
+        "username": "CoupletCoder",
+        "email": "coupletcoder@test.com",
+        "poemText": "JavaScript gives and JavaScript takes away, The coding syntax I must obey"
+    }
+];
