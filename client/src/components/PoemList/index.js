ecbb639c6fa7b3bfccf06bfaac4c7e03d90c0c0f
jossparling <jossparling@gmail.com>
2022-10-06 14:21:45 -0500

    added dummy pages

--- client/src/components/PoemList/index.js
+++ client/src/components/PoemList/index.js
@@ -1,33 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const PoemList = ({ poems, title}) => {
-    if (!poems.length) {
-      return <h3>No Poems Yet</h3>
-    }
-
+function PoemList() {
     return(
-        <div>
-            <h3>{title}</h3>
-            {poems && poems.map(poem => (
-                <div key={poem._id} className="card mb-3">
-                    <p className="card-header">
-                    <Link to={`/profile/${poem.username}`} style={{ fontWeight: 700 }} className="text-light">{poem.username}</Link>
-                        {' '}poem on {poem.createdAt}
-                    </p>
-                    <div className="card-body">
-                        <Link to={`/poem/${poem._id}`}>
-                        <p>{poem.poemText}</p>
-                        <p className="mb-0">
-                            Riffs: {poem.riffCount} || Click to{' '}
-                            {poem.riffCount ? 'see': 'start'} the discussion!
-                        </p>
-                        </Link>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
-  }
+        <nav>
+            <ul>
+                <li>PoemList</li>
+            </ul>
 
-  export default PoemList
+        </nav>
+    )
+}
+export default PoemList
