60538abee5564277dbd4b1451cac7d5e4573efa1
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-10-06 19:43:23 -0500

    added riff form component

--- client/src/components/RiffForm/index.js
+++ client/src/components/RiffForm/index.js
@@ -1,11 +1,49 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_REACTION } from '../../utils/mutations';
 
-function RiffForm() {
-    return(
-        <nav>
-            <ul>
-            </ul>
+function RiffForm({ poemId }) {
+    const [riffBody, setBody] = useState('');
+    const [addRiff, { error }] = useMutation(ADD_REACTION);
 
-        </nav>
-    )
-}
+const handleChange = event => {
+  if (event.target.value.length <= 280) {
+    setBody(event.target.value);
+  }
+};
+
+const handleFormSubmit = async event => {
+    event.preventDefault();
+  
+    try {
+      // add thought to database
+      await addRiff({
+        variables: { riffBody, poemId }
+      });
+  
+      // clear form value
+      setBody('');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <textarea
+          placeholder="Riff on this poem"
+          value={riffBody}
+          onChange={handleChange}
+        ></textarea>
+
+        <button type="submit">
+          Riff
+          {error}
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default RiffForm
