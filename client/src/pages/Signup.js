48b39cd05d56098c15e602deadf5023adaef5323
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-10-06 19:04:38 -0500

    added sign up page framework

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Signup = () => {
+  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+  
+    try {
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  return (
+    <main>
+      <div>
+        <div>
+          <h4>Sign Up</h4>
+          <div>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                placeholder='Username'
+                name='username'
+                type='username'
+                id='username'
+                value={formState.username}
+                onChange={handleChange}
+              />
+              <input
+                placeholder='Email address'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button>
+                Register
+              </button>
+            </form>
+            {error && <div>Registration failed</div>}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Signup;
