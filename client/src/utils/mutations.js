5dfe824cdc02d0394ba68600189737a6dcb41d7b
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-10-06 20:15:36 -0500

    added mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_POEM = gql`
+  mutation addPoem($poemText: String!) {
+    addThought(poemText: $thoughtText) {
+      _id
+      poemText
+      createdAt
+      username
+      riffCount
+      riffs {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_RIFF = gql`
+  mutation addRiff($poemId: ID!, $riffBody: String!) {
+    addRiff(poemId: $poemId, riffBody: $riffBody) {
+      _id
+      riffCount
+      riffs {
+        _id
+        riffBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
