a805e2a70c3eaf4965b8ebb1446866a691e2201b
Kayla Siksik <kaylasiksik@gmail.com>
2022-10-09 13:13:26 -0500

    updated RiffList

--- client/src/components/RiffList/index.js
+++ client/src/components/RiffList/index.js
@@ -1,44 +1,25 @@
-
-
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const RiffsList = ({ riffs, title }) => {
-    if (!riffs.length) {
-        return <h3>No riffs yet</h3>;
-    }
-function RiffList() {
-    return (
-        <nav>
-        <div>
-            <h3>{title}</h3>
-            {riffs &&
-                riffs.map(riff => (
-                    <div key={riff._id} className="card mb-3">
-                        <p className="card-header">
-                            <Link
-                            to={`/profile/${riff.username}`}
-                            style={{ fontWeight: 700 }}
-                            className="text-light"
-                            >
-                                {riff.username}
-                             </Link>{' '}
-                            riff on {riff.createdAt}
-                        </p>
-                        <div className="card-body">
-                        <Link to={`/riff/${riff._id}`}>
-                            <p>{riff.riffText}</p>
-                            <p className="mb-0">
-                                Reactions: {riff.reactionCount} || Click to{' '}
-                                {riff.reactionCount ? 'see' : 'start'} the discussion!
-                            </p>
-                        </Link>
-                        </div>
-                    </div>
-                ))}
+const RiffList = ({ riffs }) => {
+  return (
+    <div className="card mb-3">
+        <div className="card-header">
+            <span className="text-light">Riffs</span>
         </div>
-        </nav>
-        
-    );
-}};
-export default RiffsList;
+        <div className="card-body">
+            {riffs &&
+            riffs.map(riff => (
+            <p className="pill mb-3" key={riff._id}>
+                {reaction.reactionBody} {'// '}
+                <Link to={`/profile/${riff.username}`} style={{ fontWeight: 700 }}>
+                {riff.username} on {riff.createdAt}
+                </Link>
+            </p>
+            ))}
+        </div>
+    </div>
+  );
+};
+
+export default RiffList;
