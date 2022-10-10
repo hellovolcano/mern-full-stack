26f4f474e9ebb8bdd89f23c940facf157f307c35
jossparling <jossparling@gmail.com>
2022-10-09 20:42:48 -0500

    created header & scrapped Nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
-import Nav from '../components/Nav'
+
 import PoemList from '../components/PoemList'
 import PoemForm from '../components/PoemForm'
 
 function Home() {
     return(
         <>  
-            <Nav />
-            <h1>Syntax</h1>
+          
+            
+            
 
             <PoemForm/>
             
