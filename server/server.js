b6f20278df88c1e1c427b4a8ad3a82b3a3e13cc0
hellovolcano <valerie.gleason@gmail.com>
2022-10-11 19:44:17 -0500

    adding context to server

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ const { ApolloServer } = require('apollo-server-express')
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection')
+const { authMiddleware } = require('./utils/auth') 
 
 const PORT = process.env.PORT || 3001
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 const app = express()
 
