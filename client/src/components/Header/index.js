4a34563f03d4fe1eeccbe1ae0238bb0f47868cd9
hellovolcano <valerie.gleason@gmail.com>
2022-10-11 21:10:08 -0500

    fixing header alignment

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,9 +20,9 @@ const Header = () => {
             {Auth.loggedIn() ? (
             <>
               <li><Link to="/profile">Me</Link></li>
-              <a href="/" onClick={logout}>
+              <li><a href="/" onClick={logout}>
                 Logout
-              </a>
+              </a></li>
             </>
           ) : (
             <>
