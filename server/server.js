23ae39ec1b062b61e86bdd2cc418c23684def2cc
hellovolcano <valerie.gleason@gmail.com>
2022-10-11 21:09:18 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -28,6 +28,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app })
 }
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
